e4519fce87efa5c508e0da19a85d429425e05127
mergutm <merg@gs.utm.mx>
2025-11-04 13:11:55 -0600

    update

--- api/config/db.js
+++ api/config/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 // Conexión MongoDB 
 export async function connectMongo() {
   try {
-    console.log("🔌 Conectando a MongoDB:", process.env.MONGO_URI);
+    console.log("Conectando a MongoDB:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
